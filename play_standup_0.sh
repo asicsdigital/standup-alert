82af1f708cb7ca6dd09326a07e6e3f980ab6c72b
Judson Dunaway-Barlow <judson.dunawaybarlow@asics.com>
2018-12-07 15:41:46 -0500

    Fixed ability to be run by cron

--- play_standup_0.sh
+++ play_standup_0.sh
@@ -1,7 +1,7 @@
 #bin/sh
 
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
-mpg321 -g 100 sounds/standup_0.mp3
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
-mpg321 -g 100 sounds/make_way.mp3
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
+mpg321 -g 100 /home/pi/standup-alert/sounds/submarine_dive_alarm_short.mp3
+mpg321 -g 100 /home/pi/standup-alert/sounds/standup_0.mp3
+mpg321 -g 100 /home/pi/standup-alert/sounds/submarine_dive_alarm_short.mp3
+mpg321 -g 100 /home/pi/standup-alert/sounds/make_way.mp3
+mpg321 -g 100 /home/pi/standup-alert/sounds/submarine_dive_alarm_short.mp3
--- play_standup_10.sh
+++ play_standup_10.sh
@@ -1,5 +1,5 @@
 #bin/sh
 
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
-mpg321 -g 100 sounds/standup_10.mp3
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
+mpg321 -g 100 /home/pi/standup-alert/sounds/submarine_dive_alarm_short.mp3
+mpg321 -g 100 /home/pi/standup-alert/sounds/standup_10.mp3
+mpg321 -g 100 /home/pi/standup-alert/sounds/submarine_dive_alarm_short.mp3
