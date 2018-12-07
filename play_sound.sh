8062e7a9e27521de1f2d901d9ce11370764ae36f
Judson Dunaway-Barlow <judson.dunawaybarlow@asics.com>
2018-12-07 14:51:15 -0500

    Deleted old files, adding "doing_great"

--- play_sound.sh
+++
@@ -1,5 +0,0 @@
-#bin/sh
-
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
-mpg321 -g 100 sounds/standup_10.mp3
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
--- play_standup_now.sh
+++
@@ -1,7 +0,0 @@
-#bin/sh
-
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
-mpg321 -g 100 sounds/standup_0.mp3
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
-mpg321 -g 100 sounds/make_way.mp3
-mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
