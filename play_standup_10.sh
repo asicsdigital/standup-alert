ecc297ed2c873aa55ab5a55bdc16d6a156bd18a7
Judson Dunaway-Barlow <judson.dunawaybarlow@asics.com>
2018-12-07 14:46:46 -0500

    Separated 3 alerts into their own files

---
+++ play_standup_10.sh
@@ -0,0 +1,5 @@
+#bin/sh
+
+mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
+mpg321 -g 100 sounds/standup_10.mp3
+mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
---
+++ play_standup_5.sh
@@ -0,0 +1,5 @@
+#bin/sh
+
+mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
+mpg321 -g 100 sounds/standup_5.mp3
+mpg321 -g 100 sounds/submarine_dive_alarm_short.mp3
